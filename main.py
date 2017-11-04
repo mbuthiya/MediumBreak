ff4beb2e589feb6840c114526873c5038ba4310a
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-11-04 22:04:21 +0300

    Create read_post function Function reads links from the article txt file Shuffles the links Choses random links based on number of sessions

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!virtual/bin/python3.6
+import random
+
+
+def read_posts(sessions):
+
+    with open('articles.txt', 'r') as handle:
+        articles = handle.readlines()
+        random.shuffle(articles)
+        chosen_articles = random.sample(articles, k=sessions)
+
+    return chosen_articles
+
+    # if __name__ == '__main__':
+    #     print('Welcome To Take a break How many sessions will you be working?')
+    #     sessions = int(input())
+    #     print('How many Minutes per session?')
+    #     time_per_session = int(input())
