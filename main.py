b8ecb2b4f33a2e357a7ebbfc7926fae5b24cec06
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-11-04 22:15:15 +0300

    Created get_int_input function THis function gets Integer Input and Validates it

--- main.py
+++ main.py
@@ -16,8 +16,21 @@ def read_posts(sessions):
 def open_browser(link):
     webbrowser.open_new_tab(link)
 
-    # if __name__ == '__main__':
-    #     print('Welcome To Take a break How many sessions will you be working?')
-    #     sessions = int(input())
-    #     print('How many Minutes per session?')
-    #     time_per_session = int(input())
+
+def get_int_input():
+
+    while True:
+        try:
+            number = int(input())
+            return number
+        except ValueError:
+            print('Please Enter an Integer Input')
+            continue
+
+
+def main():
+    print('Welcome To Take a break How many sessions will you be working?')
+    sessions = get_int_input()
+
+    print('How many Minutes per session?')
+    time_per_session = get_int_input() * 60
