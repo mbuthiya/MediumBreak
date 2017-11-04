5452308c37b8c67a06ba92410b681d43ada28800
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-11-04 22:25:05 +0300

    Created Main Function Created man function that queries the user for session and time and open articles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!virtual/bin/python3.6
 import random
+import time
 import webbrowser
 
 
@@ -33,4 +34,17 @@ def main():
     sessions = get_int_input()
 
     print('How many Minutes per session?')
-    time_per_session = get_int_input() * 60
+    time_per_session = get_int_input()
+
+    posts = read_posts(sessions)
+    sessions_run = 0
+
+    while sessions_run < sessions:
+        time.sleep(time_per_session)
+        post = posts[sessions_run]
+        open_browser(post)
+        sessions_run += 1
+
+
+if __name__ == '__main__':
+    main()
